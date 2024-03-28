92d2b4753f1c5709c2cd219ec0550c40ea68a979
ruhulaminparvez <ruhulaminparvez007@gmail.com>
2024-03-28 22:49:09 +0600

    Responsive Grid

--- src/Pages/Home/clientResults/ClientResults.jsx
+++ src/Pages/Home/clientResults/ClientResults.jsx
@@ -9,7 +9,7 @@ const ClientResults = () => {
                     <h4 className="text-4xl text-center">Client Results</h4>
                     <p className="text-center py-5">Grow your followers on Instagram with genuinely interested users. Experience high <br /> engagement. Gain more customers. Don’t take our word for it, see client results for yourself!</p>
                     <div className="w-full h-full flex items-center justify-center pt-2">
-                        <div className="grid grid-cols-4 gap-8">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                             {clientResultsData?.map((item, index) => (
                                 <div className="w-60 h-44 bg-white rounded-xl shadow-xl flex flex-col items-center justify-center gap-3" key={index}>
                                     <h4 className="text-black text-3xl">{item?.title}</h4>
